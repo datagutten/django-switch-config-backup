ff3b353ddd2eebdcc9642a1dea9ea701744708d7
datagutten <datagutten@datagutten.net>
2020-02-15 13:54:20 +0100

    Raise ConnectionError when SSH connection fails

--- config_backup/switch_cli/connections/common/ssh.py
+++ config_backup/switch_cli/connections/common/ssh.py
@@ -12,11 +12,14 @@ class SSH(SwitchCli):
     def connect(self, ip, username=None, password=None):
         self.connection = paramiko.SSHClient()
         self.connection.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-        self.connection.connect(hostname=ip, username=username,
-                                password=password,
-                                look_for_keys=False,
-                                allow_agent=False)
-        self.connection = self.connection.invoke_shell()
+        try:
+            self.connection.connect(hostname=ip, username=username,
+                                    password=password,
+                                    look_for_keys=False,
+                                    allow_agent=False)
+            self.connection = self.connection.invoke_shell()
+        except paramiko.SSHException as e:
+            raise ConnectionError(e)
 
     def command(self, cmd, expected_response=None, timeout=2):
         self.connection.send(cmd + '\n')
